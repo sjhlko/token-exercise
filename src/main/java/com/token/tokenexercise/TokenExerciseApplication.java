c29a5a4340e85d757141a85946bdfc1b1f4328c7
서정희 <sjhlko@naver.com>
2022-12-06 11:42:57 +0900

    Initial commit

---
+++ src/main/java/com/token/tokenexercise/TokenExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.token.tokenexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TokenExerciseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/token/tokenexercise/TokenExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.token.tokenexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokenExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
